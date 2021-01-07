31c650dda7b5492ee5d68d58aedb19bd0e0a4844
Swatineha2312 <swatineha2312@gmail.com>
2021-01-07 19:19:33 +1100

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1,15 +1,15 @@
 -- select statment
--- USE sql_inventory;
--- SELECT name,unit_price,(unit_price*1.1) AS newprice FROM products ;
+USE sql_inventory;
+SELECT name,unit_price,(unit_price*1.1) AS newprice FROM products ;
 
 -- select clause
--- USE sql_store;
--- SELECT distinct state from customers;
+USE sql_store;
+SELECT distinct state from customers;
 
 -- where clause
- USE sql_store;
--- SELECT * FROM customers where state='VA';
--- select * from customers where customer_id=1 ;
--- SELECT * from customers where state <> 'VA'; -- this gives details of all states except VA
--- select * from customers where birth_date>'1990-01-01'; -- date is also presented in inverted col
--- SELECT * from orders where order_date>='2019-01-01'; -- to check the orders placed in that year
+USE sql_store;
+SELECT * FROM customers where state='VA';
+select * from customers where customer_id=1 ;
+SELECT * from customers where state <> 'VA'; -- this gives details of all states except VA
+select * from customers where birth_date>'1990-01-01'; -- date is also presented in inverted col
+SELECT * from orders where order_date>='2019-01-01'; -- to check the orders placed in that year
