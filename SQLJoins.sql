861e7b84b97603a7d0b23a9c1e7cfbd87a2ddfc6
Swatineha2312 <swatineha2312@gmail.com>
2021-01-14 16:26:25 +1100

    Update SQLJoins.sql

--- SQLJoins.sql
+++ SQLJoins.sql
@@ -34,3 +34,46 @@ SELECT e.employee_id, e.first_name, m.first_name AS manager
 FROM employees e
 JOIN employees m
  ON e.reports_to=m.employee_id
+ 
+-- joining multiple tables
+SELECT 
+o.order_id,
+o.order_date,
+c.first_name,
+c.last_name,
+os.name AS status
+FROM orders o
+JOIN customers c
+ ON o.customer_id=c.customer_id
+JOIN order_statuses os
+ ON o.status=os.order_status_id
+  
+-- Exercise to join 3 tables payment,payment_method and clients to give the payment details of clients
+USE sql_invoicing;
+SELECT 
+p.client_id,
+c.name,
+c.phone,
+p.date,
+p.amount,
+p.invoice_id,
+p.payment_method AS payment_method_id,
+p.payment_id,
+pm.name AS payment_method
+FROM payments p
+JOIN payment_methods pm
+ ON  p.payment_method=pm.payment_method_id
+JOIN clients c
+ ON p.client_id=c.client_id
+ 
+-- Comppound join
+USE sql_store;
+SELECT *  
+FROM order_items oi
+JOIN order_item_notes oin
+  ON oi.order_id=oin.order_id
+  AND oi.product_id=oin.product_idorder_item_notes
+ 
+
+
+ 
