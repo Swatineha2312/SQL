bedb6eb9f4ac8df2c8bb9b0183e2fb01330e9252
Swatineha2312 <swatineha2312@gmail.com>
2021-01-15 23:44:41 +1100

    Update SQLJoins.sql

--- SQLJoins.sql
+++ SQLJoins.sql
@@ -65,7 +65,7 @@ JOIN payment_methods pm
  ON  p.payment_method=pm.payment_method_id
 JOIN clients c
  ON p.client_id=c.client_id
- 
+
 -- Comppound join
 USE sql_store;
 SELECT *  
@@ -73,7 +73,4 @@ FROM order_items oi
 JOIN order_item_notes oin
   ON oi.order_id=oin.order_id
   AND oi.product_id=oin.product_idorder_item_notes
- 
-
-
  
